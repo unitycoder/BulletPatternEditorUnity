98ca3fc8a500a98e9d5136e11d8a5f6ab56addda
unitycoder <support@unitycoder.com>
2016-03-27 18:14:52 +0800

    converted Player.js to c#, added Bullet tag

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+   public  float speed = 24f;
+
+    int hitCount = 0;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var targetVelocity = new Vector3(0, Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        targetVelocity = transform.TransformDirection(targetVelocity);
+        targetVelocity *= speed;
+
+        var velocityChange = (targetVelocity - rb.velocity);
+
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            hitCount++;
+            Debug.Log("Hit " + hitCount + " times!");
+        }
+    }
+}
